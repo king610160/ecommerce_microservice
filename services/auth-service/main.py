2e5592f62b237892daa55c5d73873905d4b47251
king610160 <king610160@gmail.com>
2026-03-20 15:14:22 +0800

    some change

--- services/auth-service/main.py
+++ services/auth-service/main.py
@@ -28,7 +28,7 @@ def get_db():
     finally:
         db.close()
 
-# 登入頁面
+# 登入API
 @app.post("/login")
 def login(request: LoginRequest, db: Session = Depends(get_db)):
     user = db.query(User).filter(User.username == request.username).first()
